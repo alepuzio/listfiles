6da459432bbe047812229d81d04fa5a3fc5a6052
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2020-09-10 12:39:32 +0200

    fixing duplciated files in report

--- main.py
+++ main.py
@@ -1,52 +1,23 @@
 import sys
-import os
-import datetime
-#import settings
-#import message
 
-from SingleFile import SingleFile
 
-from collections import Counter
 from rootpath import Rootpath
-from report import Report
+from report_data import MapReport
+from report_data import Report
 from report_name import ReportName
+
+from duplicated_files import Duplicated
 
 def main():
     opts = sys.argv
     print( "The arguments are {0}".format ( str(opts) ) )
     read_files = Rootpath ( opts ).files()
     
-    Report ( ReportName().all() , read_files ).write()
+    Report ( ReportName().all() ).write(read_files) 
+   
+    duplicated = Duplicated(read_files).files()
     
-    map_files =  {}
-    for file_tmp in read_files:
-        map_files[file_tmp] = file_tmp.name().name() 
-    print ("map:{0}".format( str (map_files) ) )
-    duplicated = {}
-    for filename_tmp in  map_files.values(): 
-
-        value = Counter(map_files.values()) [filename_tmp] 
-        if 1 < value:
-            print(  filename_tmp + "->"+  str( value ) ) 
-            duplicated [filename_tmp] = [k for k,v in map_files.items() if v == filename_tmp ]
-            print("duplicated: " + str ( duplicated ) )
-
-    MapReport ( ReportName().duplicated() , duplicated).write()
-    '''
-    unique_read_files = []
-    duplicated_read_files = []
-    for file_tmp in read_files:
-        print("unique file {0} ".format( str( unique_read_files ) ) ) 
-        if file_tmp.name() in unique_read_files[file_tmp]:
-            duplicated_read_files.append ( file_tmp)
-            print("duplicated file {0} in {1}".format( file_tmp.name(), str (unique_read_files) ) )
-        else:
-            unique_read_files.append ( file_tmp)
-        
-    Report ( ReportName().duplicated() , duplicated_read_files).write()
-
-    Report ( ReportName().unique() , unique_read_files ).write()
-    '''
-
+    MapReport ( Report ( ReportName().duplicated() )) .write   (  duplicated     )
+    
 if __name__ == "__main__":
     main()
--- report_name.py
+++ report_name.py
@@ -11,12 +11,16 @@ class ReportName:
 
     def unique(self):
         '''@return the name of final report file of the files (once written)'''
-        return "{0}{1}.{2}".format("unique", datetime.datetime.now().strftime(self.DATE_FORMAT), self.extension )
+        return "report_unique-{0}.{1}".format( self.time(), self.extension )
 
     def duplicated(self):
         '''@return the name of final report file of the duplicated files'''
-        return "{0}{1}.{2}".format("duplicated", datetime.datetime.now().strftime(self.DATE_FORMAT), self.extension )
+        return "report_duplicated-{0}.{1}".format( self.time(), self.extension )
 
     def all(self):
         '''@return the name of final report file'''
-        return "{0}{1}.{2}".format("All", datetime.datetime.now().strftime(self.DATE_FORMAT), self.extension )
+        return "report_all-{0}.{1}".format( self.time(), self.extension )
+
+    
+    def time(self):
+        return  datetime.datetime.now().strftime(self.DATE_FORMAT)
