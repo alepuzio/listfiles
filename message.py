7e5b9f0e7c026b8b5ae6a8879a7e8a0ea7b764b9
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2020-09-11 15:40:57 +0200

    deleted useless files

--- message.py
+++
@@ -1,3 +0,0 @@
-ARGUMENTS="The arguments are: "
-INPUT_PATH_MISSING="At leat it need one argument, absolute path of root directory"
-INPUT_PATH_UNCORRECT='First parameter has to be an absolute directory'
